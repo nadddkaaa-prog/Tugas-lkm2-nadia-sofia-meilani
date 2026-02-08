33e9f937c702e596f03f1feaf145f0b733acf8a6
nadddkaaa-prog <nadddkaaa@gmail.com>
2026-02-09 01:35:48 +0700

    Add files via upload

---
+++ src/pages/About.jsx
@@ -0,0 +1,21 @@
+export default function About() { 
+return ( 
+<section className="section"> 
+<h2>Tentang Saya</h2> 
+<p> 
+Saya adalah siswa kelas XI Rekayasa 
+Perangkat Lunak yang memiliki minat 
+di bidang pemrograman web dan pengembangan 
+aplikasi. 
+</p> 
+BIODATA 
+<ul className="list"> 
+<li>Nama: 
+Nadia Sofia Meilani </li> 
+<li>Kelas: XI RPL A</li> 
+<li>Sekolah: 
+SMK PUSDIKHUBAD CIMAHI</li> 
+</ul> 
+</section> 
+  ); 
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import Hero from "../components/Hero.jsx"; 
+import SkillCard from "../components/SkillCard"; 
+export default function Home() { 
+return ( 
+<> 
+<Hero /> 
+<section id="skill" className="section"> 
+<h2>Keahlian Saya</h2> 
+<div className="grid"> 
+<SkillCard title="HTML & CSS" 
+desc="Membuat tampilan website modern" icon="  🪱  " 
+/> 
+<SkillCard title="JavaScript" 
+desc="Logika dan interaksi web" icon="  💕 " /> 
+<SkillCard title="React" desc="Single 
+Page Application" icon="   🌷 " /> 
+<SkillCard title="Laravel" 
+desc="Backend & API" icon="  😶‍🌫️   " /> 
+</div> 
+</section> 
+</> 
+); 
+} 
